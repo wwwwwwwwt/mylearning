9bfe2005ddc3462c8f8fe784d30aef8ce196e401
wwwwwwwwt <1162933894@qq.com>
2023-05-20 10:00:21 +0800

    update algorithm

---
+++ DesignMode/decorate.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: zzzzztw
+ * @Date: 2023-05-19 17:16:07
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2023-05-19 17:17:26
+ * @FilePath: /myLearning/DesignMode/decorate.cpp
+ */
+#include <memory>
+#include <iostream>
+using namespace std;
+
+class tansform{
+
+public:
+    virtual void move() = 0;
+
+};
